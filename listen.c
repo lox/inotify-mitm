46da705bb3db4c110ea4e1c957ad6b0dfab6109e
Paul Annesley <paul@annesley.cc>
2015-07-19 20:43:31 -0700

    Unused #include <...> deleted.

--- listen.c
+++ listen.c
@@ -1,12 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <errno.h>
 #include <string.h>
-#include <netdb.h>
-#include <sys/types.h>
-#include <netinet/in.h>
-#include <sys/socket.h>
 #include <arpa/inet.h>
 #include "jsmn/jsmn.h"
 #include "listen.h"
--- read_listen.c
+++ read_listen.c
@@ -1,7 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
-#include <strings.h>
 #include "listen.h"
 
 int main(int argc, char *argv[]) {
